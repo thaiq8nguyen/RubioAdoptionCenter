e81d5489399906a5c71474d10b657b6fe0e047c2
Thai Nguyen <discoverylight@yahoo.com>
2019-06-29 13:15:38 -0700

    added more ESLint rules to warn about hooks added dropdowns to display animals and breeds

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+//mport Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
-      <h1 id="pet-adoption">Rubio's Adoption Center</h1>
-      <Pet name="Bobby" animal="Dog" breed="Boo" />
-      <Pet name="Megan" animal="Dog" breed="Mixed" />
-      <Pet name="Annie" animal="Cat" breed="White" />
+      <h1 id="pet-adoption">Rubios Adoption Center</h1>
+      <SearchParams />
     </div>
   );
 };
